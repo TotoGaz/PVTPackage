5d2e9a078ae1df628fabbc1fb398af253cdcfe6f
Thomas Gazzola <thomas.gazzola@total.com>
2020-09-01 17:05:13 -0700

    Test is working.

--- PVTPackage/source/refactor/testFirst.cpp
+++ PVTPackage/source/refactor/testFirst.cpp
@@ -1,19 +1,199 @@
-// This include is necessary for nlohmann::json to find the proper serializers
-#include "refactor/serializers/SerializersDeserializers.hpp"
-
-//#include "MultiphaseSystem/MultiphaseSystemProperties.hpp"
-//
-//#include "MultiphaseSystem/PhaseModel/CubicEOS/CubicEoSPhaseModel.hpp"
-//#include "MultiphaseSystem/PhaseSplitModel/NegativeTwoPhaseFlash.hpp"
-
 #include "refactor/passiveDataStructures/MultiphaseSystemProperties.hpp"
+
+// This include is necessary for nlohmann::json to find the proper serializers
 #include "refactor/serializers/MultiphaseSystemProperties.hpp"
 
-#include <gtest/gtest.h>
+#include "MultiphaseSystem/PhaseModel/CubicEOS/CubicEoSPhaseModel.hpp"
+#include "MultiphaseSystem/PhaseSplitModel/NegativeTwoPhaseFlash.hpp"
+#include "MultiphaseSystem/MultiphaseSystemProperties.hpp"
 
+#include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
 
+#include <algorithm>
+
 using json = nlohmann::json;
+
+PVTPackage::PHASE_STATE convert( const PVTPackage::pds::PHASE_STATE & input ){
+  using namespace PVTPackage;
+
+  const std::map< pds::PHASE_STATE, PHASE_STATE > m{
+    { pds::PHASE_STATE::UNKNOWN,       PHASE_STATE::UNKNOWN },
+    { pds::PHASE_STATE::GAS,           PHASE_STATE::GAS },
+    { pds::PHASE_STATE::OIL,           PHASE_STATE::OIL },
+    { pds::PHASE_STATE::WATER,         PHASE_STATE::WATER },
+    { pds::PHASE_STATE::OIL_GAS,       PHASE_STATE::OIL_GAS },
+    { pds::PHASE_STATE::GAS_WATER,     PHASE_STATE::GAS_WATER },
+    { pds::PHASE_STATE::OIL_WATER,     PHASE_STATE::OIL_WATER },
+    { pds::PHASE_STATE::OIL_GAS_WATER, PHASE_STATE::OIL_GAS_WATER }
+  };
+
+  return m.at( input );
+}
+
+PVTPackage::PHASE_TYPE convert( const PVTPackage::pds::PHASE_TYPE & input ){
+  using namespace PVTPackage;
+
+  const std::map< pds::PHASE_TYPE, PHASE_TYPE > m{
+    { pds::PHASE_TYPE::LIQUID_WATER_RICH, PHASE_TYPE::LIQUID_WATER_RICH },
+    { pds::PHASE_TYPE::OIL,               PHASE_TYPE::OIL },
+    { pds::PHASE_TYPE::GAS,               PHASE_TYPE::GAS },
+    { pds::PHASE_TYPE::UNKNOWN,           PHASE_TYPE::UNKNOWN }
+  };
+
+  return m.at( input );
+}
+
+PVTPackage::EOS_TYPE convert( const PVTPackage::pds::EOS_TYPE & input ){
+  using namespace PVTPackage;
+
+  const std::map< pds::EOS_TYPE, EOS_TYPE > m{
+    { pds::EOS_TYPE::REDLICH_KWONG_SOAVE, EOS_TYPE::REDLICH_KWONG_SOAVE },
+    { pds::EOS_TYPE::PENG_ROBINSON,       EOS_TYPE::PENG_ROBINSON },
+    { pds::EOS_TYPE::UNKNOWN,             EOS_TYPE::UNKNOWN }
+  };
+
+  return m.at( input );
+}
+
+std::vector< PVTPackage::PHASE_TYPE > convert( const std::vector< PVTPackage::pds::PHASE_TYPE > & input )
+{
+  using namespace PVTPackage;
+
+  // FIXME I cannot give raw function to std::transform
+  auto f = []( const pds::PHASE_TYPE & i ) {
+    return convert( i );
+  };
+
+  std::vector< PHASE_TYPE > output( input.size() );
+  std::transform( input.cbegin(), input.cend(), output.begin(), f );
+
+  return output;
+}
+
+PVTPackage::ComponentProperties convert( const PVTPackage::pds::ComponentProperties & input )
+{
+  return PVTPackage::ComponentProperties{ input.NComponents, input.Label, input.Mw, input.Tc, input.Pc, input.Omega };
+}
+
+std::shared_ptr< PVTPackage::PhaseModel > convert( const std::shared_ptr< PVTPackage::pds::PhaseModel > input )
+{
+  using namespace PVTPackage;
+
+  if (auto pm = std::dynamic_pointer_cast< const pds::CubicEoSPhaseModel >( input )){
+    auto output = std::make_shared< CubicEoSPhaseModel >(
+                                  convert( pm->m_ComponentsProperties ),
+                                  convert( pm->m_EOSType ),
+                                  convert( pm->m_PhaseType )
+                                  );
+    // TODO is this enough?
+    return output;
+  }
+  std::cerr << "Not fully implemented | PVTPackage::PhaseModel convert( const PVTPackage::pds::PhaseModel & input )" << std::endl;
+  exit(1);
+}
+
+template< typename T >
+void compare( const PVTPackage::ScalarPropertyAndDerivatives< T > & actual,
+              const PVTPackage::pds::ScalarPropertyAndDerivatives< T > & expected, double eps )
+{
+  ASSERT_NEAR( actual.value, expected.value, eps );
+  ASSERT_NEAR( actual.dT, expected.dT, eps );
+  ASSERT_NEAR( actual.dP, expected.dP, eps );
+
+  ASSERT_EQ( actual.dz.size(), expected.dz.size() );
+  auto f = [eps]( double a, double b )
+  {
+    return std::abs( a - b ) < eps;
+  };
+  bool test = std::equal( actual.dz.cbegin(), actual.dz.cend(), expected.dz.cbegin(), expected.dz.cend(), f );
+  EXPECT_TRUE( test );
+}
+
+template< typename T >
+void compare( const PVTPackage::VectorPropertyAndDerivatives< T > & actual,
+              const PVTPackage::pds::VectorPropertyAndDerivatives< T > & expected, double eps )
+{
+  auto f = [eps]( double a, double b )
+  {
+    return std::abs( a - b ) < eps;
+  };
+
+  {
+    ASSERT_EQ( actual.value.size(), expected.value.size() );
+    bool test = std::equal( actual.value.cbegin(), actual.value.cend(),
+                            expected.value.cbegin(), expected.value.cend(), f );
+    EXPECT_TRUE( test );
+  }
+//  {
+//    ASSERT_EQ( actual.dP.size(), expected.dP.size() );
+//    bool test = std::equal( actual.dP.cbegin(), actual.dP.cend(),
+//                            expected.dP.cbegin(), expected.dP.cend(), f );
+//    EXPECT_TRUE( test );
+//  }
+//  {
+//    ASSERT_EQ( actual.dT.size(), expected.dT.size() );
+//    bool test = std::equal( actual.dT.cbegin(), actual.dT.cend(),
+//                            expected.dT.cbegin(), expected.dT.cend(), f );
+//    EXPECT_TRUE( test );
+//  }
+  // FIXME dz
+  ASSERT_EQ( actual.dz.size(), expected.dz.size() );
+}
+
+using PdsPhaseType2PhaseModel = std::unordered_map< PVTPackage::pds::PHASE_TYPE, std::shared_ptr< PVTPackage::pds::PhaseModel >, PVTPackage::pds::EnumClassHash >;
+using PhaseType2PhaseModel = std::unordered_map< PVTPackage::PHASE_TYPE, std::shared_ptr< PVTPackage::PhaseModel >, PVTPackage::EnumClassHash >;
+
+PhaseType2PhaseModel convert( const PdsPhaseType2PhaseModel & input )
+{
+  using namespace PVTPackage;
+
+  PhaseType2PhaseModel output;
+
+  for ( const auto & phaseType2PhaseModel: input ) {
+    const PHASE_TYPE phaseType = convert( phaseType2PhaseModel.first );
+    const std::shared_ptr< PhaseModel > phaseModel = convert( phaseType2PhaseModel.second );
+    output.insert( {phaseType, phaseModel} );
+  }
+
+  return output;
+}
+
+void compare( const PVTPackage::MultiphaseSystemProperties & actual,
+              const PVTPackage::pds::MultiphaseSystemProperties & expected )
+{
+  using namespace PVTPackage;
+
+  ASSERT_NEAR( actual.Temperature, expected.Temperature, 1.e-10 );
+  ASSERT_NEAR( actual.Pressure, expected.Pressure, 1.e-10 );
+  ASSERT_EQ( actual.PhaseState, convert( expected.PhaseState ) );
+
+  for( const auto & phaseType2PhaseProperties: expected.PhasesProperties )
+  {
+    const pds::PHASE_TYPE & pt = phaseType2PhaseProperties.first;
+    const pds::PhaseProperties & expectedPhaseProps = phaseType2PhaseProperties.second;
+    const PhaseProperties & actualPhaseProps = actual.PhasesProperties.at( convert( pt ) );
+
+    ASSERT_NEAR( actualPhaseProps.Compressibility, expectedPhaseProps.Compressibility, 1.e-10 );
+    ASSERT_NEAR( actualPhaseProps.Viscosity, expectedPhaseProps.Viscosity, 1.e-10 );
+    ASSERT_NEAR( actualPhaseProps.MassEnthalpy, expectedPhaseProps.MassEnthalpy, 1.e-10 );
+
+    compare( actualPhaseProps.MolecularWeight, expectedPhaseProps.MolecularWeight, 1.e10 );
+    compare( actualPhaseProps.CompressibilityFactor, expectedPhaseProps.CompressibilityFactor, 1.e10 );
+    compare( actualPhaseProps.MoleDensity, expectedPhaseProps.MoleDensity, 1.e10 );
+    compare( actualPhaseProps.MoleComposition, expectedPhaseProps.MoleComposition, 1.e-10 );
+    compare( actualPhaseProps.LnFugacityCoefficients, expectedPhaseProps.LnFugacityCoefficients, 1.e-10 );
+  }
+
+  for( const auto & phaseType2PhaseMoleFraction: expected.PhaseMoleFraction )
+  {
+    const pds::PHASE_TYPE & pt = phaseType2PhaseMoleFraction.first;
+    const pds::ScalarPropertyAndDerivatives< double > & expectedPhaseMoleFraction = phaseType2PhaseMoleFraction.second;
+    const ScalarPropertyAndDerivatives< double > & actualPhaseMoleFraction = actual.PhaseMoleFraction.at( convert( pt ) );
+
+    ASSERT_NEAR( expectedPhaseMoleFraction.value, actualPhaseMoleFraction.value, 1.e-10 );
+  }
+}
 
 TEST( PVTPackageRefactor, first )
 {
@@ -32,20 +212,16 @@ TEST( PVTPackageRefactor, first )
   const PVTPackage::pds::ComponentProperties & componentProperties = pm->m_ComponentsProperties;
   componentProperties.Label.size();
 
+  PVTPackage::MultiphaseSystemProperties msp( convert( refMsp.PhaseTypes ), refMsp.Feed.size() );
+  msp.Temperature = refMsp.Temperature ;
+  msp.Pressure = refMsp.Pressure ;
+  msp.Feed = refMsp.Feed ;
 
+  msp.PhaseModels = convert( refMsp.PhaseModels );
+  // the PhaseMoleFraction and PhasesProperties seem to be the results, so no need to copy.
 
-//  PVTPackage::MultiphaseSystemProperties msp( refMsp.PhaseTypes, refMsp.Feed.size() );
-//  msp.Temperature = refMsp.Temperature ;
-//  msp.Pressure = refMsp.Pressure ;
-//  msp.Feed = refMsp.Feed ;
-//
-//  msp.PhaseModels = refMsp.PhaseModels;
-//
-//  PVTPackage::NegativeTwoPhaseFlash flash( componentProperties ) ;
-//  flash.ComputeEquilibrium( msp );
+  PVTPackage::NegativeTwoPhaseFlash flash( convert( componentProperties ) );
+  flash.ComputeEquilibrium( msp );
 
-  j1.size();
-
-  EXPECT_EQ( true, false );
-
+  compare( msp, refMsp );
 }
